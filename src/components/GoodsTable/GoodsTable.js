9ffdc2f296bc8ec3e5f363497a199b58732354b0
grantge <grantfromgeorgia@gmail.com>
2021-03-29 01:03:39 +0300

    added table add cart

--- src/components/GoodsTable/GoodsTable.js
+++ src/components/GoodsTable/GoodsTable.js
@@ -1,9 +1,36 @@
 import React from 'react';
 
+import './GoodsTable.css';
+
 function GoodsTable() {
   return (
     <div className="nes-container with-title is-centered">
-      <div className="flex-container"></div>
+      <p className="title">Weapon</p>
+      <div className="container">
+        <div className="nes-container is-rounded is-dark">
+          <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+        </div>
+
+        <div className="nes-container is-rounded is-dark">
+          <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+        </div>
+
+        <div className="nes-container is-rounded is-dark">
+          <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+        </div>
+
+        <div className="nes-container is-rounded is-dark">
+          <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+        </div>
+
+        <div className="nes-container is-rounded is-dark">
+          <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+        </div>
+
+        <div className="nes-container is-rounded is-dark">
+          <p>Good morning. Thou hast had a good night's sleep, I hope.</p>
+        </div>
+      </div>
     </div>
   );
 }
